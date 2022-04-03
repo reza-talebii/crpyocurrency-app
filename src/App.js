fc5766b912b382f4909f1e7d62e43d2407e801e8
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-03 18:40:07 +0430

    add routes main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 //IMPORT ANTD COMPONENTS
 import { Layout, Typography, Space } from "antd";
 
 //IMPORT COMPONENTS
-import { Navbar } from "./Components/index";
+import {
+  Navbar,
+  HomePage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./Components/index";
 
 import "./App.css";
 
@@ -15,7 +22,17 @@ function App() {
       <nav className={"navbar"}>
         <Navbar />
       </nav>
-      <main className="main"></main>
+      <main className="main">
+        <Layout>
+          <div className="routes">
+            <Route path="/" element={<HomePage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinID" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} />
+          </div>
+        </Layout>
+      </main>
       <footer className="footer"></footer>
     </section>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -8,6 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Router>
-    <App />
+    <Routes>
+      <App />
+    </Routes>
   </Router>
 );
