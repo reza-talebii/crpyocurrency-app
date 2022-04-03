587e46faa654d25e3f0ee9da387b7c228102b479
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-03 18:16:54 +0430

    create index components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //IMPORT ANTD COMPONENTS
 import { Layout, Typography, Space } from "antd";
 
 //IMPORT COMPONENTS
-import Navbar from "./Components/Navbar";
+import { Navbar } from "./Components/index";
 
 import "./App.css";
 
 function App() {
   return (
     <section className="App">
-      <nav className={"navbar"}></nav>
+      <nav className={"navbar"}>
+        <Navbar />
+      </nav>
       <main className="main"></main>
       <footer className="footer"></footer>
     </section>
