23efb568b203d7ad8466a7483dbd42487a251ff5
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-03 18:34:02 +0430

    complete menu Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import icon from "../assets/images/icon.png";
+import icon from "../../assets/images/icon.png";
 import "./Navbar.css";
 
 import { Button, Menu, Typography, Avatar } from "antd";
@@ -8,9 +8,9 @@ import { Link } from "react-router-dom";
 import {
   HomeOutlined,
   MoneyCollectOutlined,
-  BulbFilled,
   FundOutlined,
   MenuOutlined,
+  BulbOutlined,
 } from "@ant-design/icons";
 
 const Navbar = () => {
@@ -21,6 +21,20 @@ const Navbar = () => {
         <Typography.Title level={3} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
         {/* <Button className="menu-control-container"></Button> */}
       </div>
     </div>
