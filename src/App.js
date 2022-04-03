a769760f6401d2a2bba0f7ec1d533e17914247fd
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-03 17:51:01 +0430

    install pakages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className={"navbar"}></div>
+    </div>
+  );
 }
 
 export default App;
