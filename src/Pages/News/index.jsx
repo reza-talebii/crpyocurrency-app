34b12372d99e38572472c85f37f44d87948b35e1
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-04 11:30:12 +0430

    cryptoNewApi add store

--- src/Pages/News/index.jsx
+++ src/Pages/News/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
-// import {Select,};
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
-const News = () => {
+import { useGetCryptoNewsQuery } from "../../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "cryptocurrency",
+    count,
+  });
+
+  console.log(cryptoNews);
+
   return <div>News</div>;
 };
 
