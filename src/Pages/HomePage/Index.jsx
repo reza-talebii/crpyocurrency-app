d91de2be58441a41d4b77d29484c6fdd44ec3dd9
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-04 12:34:13 +0430

    complete news page

--- src/Pages/HomePage/Index.jsx
+++ src/Pages/HomePage/Index.jsx
@@ -63,7 +63,7 @@ const HomePage = () => {
           Latest Crypto News
         </Title>
         <Title level={3} className="show-more">
-          <Link to="/cryptoCurrencies">Show More</Link>
+          <Link to="/news">Show More</Link>
         </Title>
       </section>
       <News simplified />
