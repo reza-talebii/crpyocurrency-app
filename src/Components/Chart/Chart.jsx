9304be6f28a3d42f99a4b352dd05f1ee717b524d
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-25 17:38:51 +0430

    create chart files

---
+++ src/Components/Chart/Chart.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import LineChart from "./LineChart";
+// import TradingviewChart from "./TradingviewChart";
+
+import { Col, Row, Typography } from "antd";
+const { Title } = Typography;
+
+const Chart = ({ coinHistory, currentPrice, coinName }) => {
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Chart;
