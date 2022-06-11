841683e35aaf09842c1844beeea22caacb90610a
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-06-11 19:37:31 +0430

    init project

--- src/Components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as CryptoDetails } from "../Pages/CryptoDetails";
-export { default as Cryptocurrencies } from "../Pages/Cryptocurrencies";
-export { default as Exchanges } from "../Pages/Exchanges";
-export { default as HomePage } from "../Pages/HomePage/Index";
-export { default as News } from "../Pages/News";
-export { default as Loader } from "../Components/Loader";
-export { default as Error } from "../Components/Error";
