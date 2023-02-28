d6693a654abc8426f2fc1ffd4b29f71b4a412c4d
reza_talebi <reza.talebi0902@gmail.com>
2023-02-28 15:25:27 +0330

    delete packages

--- src/common/interfaces/Routes.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-export type RoutesObject = {
-  [name: string]: {
-    get: string;
-    return: ReactElement;
-    title: string;
-  };
-};
--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as CryptoDetails } from "./CryptoDetails";
-export { default as Cryptocurrencies } from "./Cryptocurrencies";
-export { default as Exchanges } from "./Exchanges";
-export { default as HomePage } from "./HomePage";
-export { default as News } from "./News";
